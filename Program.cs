4635a6d2276ef08966810f8b14651b9525eb3ba0
Vincent Biret <vincentbiret@hotmail.com>
2022-10-06 15:43:19 -0400

    - adds creation of tasks
    Signed-off-by: Vincent Biret <vincentbiret@hotmail.com>

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using GithubTodoDemo.GitHub;
 using GithubTodoDemo.MicrosoftGraph;
+using GithubTodoDemo.MicrosoftGraph.Models;
 
 Console.WriteLine("Hello, World!");
 
@@ -9,3 +10,21 @@ var pullRequests = await gitHubClient.Repos["baywet"]["demo"].Pulls.GetAsync();
 var graphClient = new MicrosoftGraphClient(null);
 var todoLists = await graphClient.Me.Todo.Lists.GetAsync();
 var todoList = todoLists.Value.First();
+
+foreach(var pullRequest in pullRequests) {
+	var addedTask = await graphClient.Me.Todo.Lists[todoList.Id].Tasks.PostAsync(
+		new TodoTask() {
+			Title = pullRequest.Title,
+			DueDateTime = new DateTimeTimeZone {
+				DateTime = pullRequest.Created_at.Value.Add(TimeSpan.FromDays(7)).ToString("o"),
+				TimeZone = "UTC"
+			},
+			Importance = Importance.High,
+			LinkedResources = new List<LinkedResource> {
+				new LinkedResource {
+					WebUrl = pullRequest.Html_url
+				}
+			}
+	});
+	Console.WriteLine($"Added task {addedTask.Title} to your todo list");
+}
