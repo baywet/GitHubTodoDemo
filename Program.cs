1fd6d8c12134659f5c982cd43e4ff49e9831782d
Vincent Biret <vincentbiret@hotmail.com>
2022-10-06 15:34:32 -0400

    - adds basic code to get the PRs and create the tasks

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using GithubTodoDemo.GitHub;
+using GithubTodoDemo.MicrosoftGraph;
+
 Console.WriteLine("Hello, World!");
+
+var gitHubClient = new GitHubClient(null);
+var pullRequests = await gitHubClient.Repos["baywet"]["demo"].Pulls.GetAsync();
+var graphClient = new MicrosoftGraphClient(null);
+var todoLists = await graphClient.Me.Todo.Lists.GetAsync();
+var todoList = todoLists.Value.First();
