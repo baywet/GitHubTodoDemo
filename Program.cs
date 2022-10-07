d243a0f62f054687819d9f641e48750f66b728e7
Vincent Biret <vincentbiret@hotmail.com>
2022-10-07 08:21:53 -0400

    - adds microsoft graph authentication
    Signed-off-by: Vincent Biret <vincentbiret@hotmail.com>

--- Program.cs
+++ Program.cs
@@ -1,13 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Azure.Identity;
 using GithubTodoDemo.GitHub;
 using GithubTodoDemo.MicrosoftGraph;
 using GithubTodoDemo.MicrosoftGraph.Models;
+using Microsoft.Kiota.Authentication.Azure;
+using Microsoft.Kiota.Http.HttpClientLibrary;
 
 Console.WriteLine("Hello, World!");
 
 var gitHubClient = new GitHubClient(null);
 var pullRequests = await gitHubClient.Repos["baywet"]["demo"].Pulls.GetAsync();
-var graphClient = new MicrosoftGraphClient(null);
+
+var tokenCredential = new DeviceCodeCredential(
+    clientId: "f19e2a30-d500-4fa7-8582-bd6099088b37",
+    tenantId: "09988f3c-947e-4986-a87e-37ad49a3b175",
+    deviceCodeCallback: (deviceCodeInfo, _) =>
+    {
+        Console.WriteLine(deviceCodeInfo.Message);
+        return Task.FromResult(0);
+});
+var graphAuthenticationProvider = new AzureIdentityAuthenticationProvider(tokenCredential, new string[] {"graph.microsoft.com"}, scopes: new string[] { "Tasks.ReadWrite"});
+var graphRequestAdapter = new HttpClientRequestAdapter(graphAuthenticationProvider);
+
+var graphClient = new MicrosoftGraphClient(graphRequestAdapter);
 var todoLists = await graphClient.Me.Todo.Lists.GetAsync();
 var todoList = todoLists.Value.First();
 
