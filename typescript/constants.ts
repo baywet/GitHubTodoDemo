0f81313f66bf35bdc62999a2db6593bf2bf25d10
Vincent Biret <vincentbiret@hotmail.com>
2024-10-15 14:10:36 -0400

    chore: adds constants for auth

---
+++ typescript/constants.ts
@@ -0,0 +1,3 @@
+export const GraphTenantId = "09988f3c-947e-4986-a87e-37ad49a3b175";
+export const GraphClientId = "f19e2a30-d500-4fa7-8582-bd6099088b37";
+export const GithubClientId = "62e33d5226194c3dfbd4";
